3da1d019fcf5ca6c7b0e9ac30ba97bd861c16501
hp <namstudionsk@gmail.com>
2017-12-29 01:01:07 +0700

    fix lv

--- exe.sh
+++ exe.sh
@@ -4,30 +4,7 @@
 #		Hello world bash
 #
 #
-echo "ls -l"
-echo 'ls -l'
-echo `ls -l`
-#
-#	wtf
-#
-echo "echo \"42\""
-echo "echo '42'"
-echo "echo \'42\'"
-echo 'echo "42"'
-echo `echo 42`
-#
-#	Vars
-#
-var1="hello"
-echo $var1
-var2='world'
-echo $var2
-var3="Hello\" \" World!"
-echo $var3
-var4="Hi Russia '0_o' !!!"
-echo $var4
-var5="@Hi Novosibirsk #\" ##\" ###\'####\' \\ "
-echo $var5
+echo "Hi"
 #
 #	True Vars
 #
